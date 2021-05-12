042220bd83f8cf4544eeb5a19c1883e4fe361ea3
yuanweibo <2435722121@qq.com>
2021-05-12 11:31:44 +0800

    配置请求拦截、响应拦截、baseURL

--- money-admin/src/api/user.js
+++ money-admin/src/api/user.js
@@ -2,7 +2,12 @@ import request from '@/utils/request'
 
 export function login() {
   return request({
-    url: '/login'
+    url: '/login',
+    method: 'post',
+    params: {
+      userName: 'admin',
+      passWord: '123456'
+    }
   })
 }
 
--- money-admin/src/utils/request.js
+++ money-admin/src/utils/request.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:3300',
+  baseURL: 'http://127.0.0.1:3399/admin',
   timeout: 5000
+})
+
+service.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+  return config
+})
+
+service.interceptors.response.use(response => {
+  return response
 })
 
 export default service
