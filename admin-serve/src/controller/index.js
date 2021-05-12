6f5766d0c7991b8dcbe4e20eff649f1ebd5b6780
yuanweibo <2435722121@qq.com>
2021-05-12 11:27:05 +0800

    完成登录功能

--- admin-serve/src/controller/index.js
+++ admin-serve/src/controller/index.js
@@ -1,16 +1,28 @@
-class Process {
-    static users = require('./user/index')
+const users = require('./user/index')
+const Token = require('./token/index')
 
-    static async login(req, res) {
-      const { userName, passWord } = req.query
+const login = async(req, res) => {
+  const { userName, passWord } = req.query
 
-      // 查询是否有该用户，密码是否正确
-      const message = await Process.users.login(userName, passWord)
+  // 查询是否有该用户，密码是否正确
+  const message = await users.login(userName, passWord)
 
-      res.send(message)
-    }
+  res.send(message)
 }
 
+// const abc = async(req, res) => {
+// let token = req.headers.authorization
+
+// token = token.split(' ')[1]
+
+// Token.verifyToken(token).then((res1) => {
+//   console.log(res1)
+// }).catch(err => {
+//   console.log(err)
+// })
+//
+//   // res.send('123')
+// }
 module.exports = {
-  login: Process.login
+  login
 }
